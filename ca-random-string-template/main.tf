5ee43913faadc780042c3779085b06d3bd2a29ea
donglev91 <donglev91.work@gmail.com>
2019-09-20 14:08:40 +0700

    Create main.tf
    random

---
+++ ca-random-string-template/main.tf
@@ -0,0 +1,23 @@
+resource "random_string" "random_name_1" {
+	length  = 10
+	special = false
+	lower   = false
+}
+
+resource "random_string" "random_name_2" {
+	length  = 10
+	special = false
+	lower   = false
+}
+
+resource "random_string" "random_name_3" {
+	length  = 10
+	special = false
+	lower   = false
+}
+
+resource "random_string" "random_name_4" {
+	length  = 10
+	special = false
+	lower   = false
+}
