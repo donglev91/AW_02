54908c9ab61ee1934aecb1cedc49a566957e9a17
donglev91 <donglev91.work@gmail.com>
2019-09-20 15:29:15 +0700

    Update main.tf

--- ca-aws-template/main.tf
+++ ca-aws-template/main.tf
@@ -17,3 +17,19 @@ resource "aws_instance" "cda_instance" {
 output "public_port" {
   value = "${aws_instance.cda_instance.public_ip}"
 }
+
+variable "access_key" {
+  default = "your aws access key"
+}
+
+variable "secret_key" {
+  default = "your aws secret key"
+}
+
+variable "aws_ami" {
+	default = "ami-20dd9172"
+}
+
+variable "aws_security_group_id" {
+	default = "sg-0cd9d595d48c30e08"
+}
