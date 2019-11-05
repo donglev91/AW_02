4b68cb8bc7504709c3989c092f353f513f5d594c
Dong <dong.viet@broadcom.com>
2019-11-05 14:47:55 +0700

    aws-cda providers template

---
+++ aws-cda/main.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+  region     = "ap-southeast-1"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+}
+
+resource "aws_instance" "cda_instance" {
+  ami                    = "${var.aws_ami}"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["sg-fef1f999"]
+}
+
+output "public_port" {
+  value = "${aws_instance.cda_instance.public_ip}"
+}
+
+variable "access_key" {
+  default = "your aws access key"
+}
+
+variable "secret_key" {
+  default = "your aws secret key"
+}
+
+variable "aws_ami" {
+	default = "ami-20dd9172"
+}
+
+variable "aws_security_group_id" {
+	default = "sg-0cd9d595d48c30e08"
+}
+
+provider "cda" {
+  cda_server     = "${var.cda_server}"
+  user           = "${var.cda_user}"
+  password       = "${var.cda_password}"  
+	
+  default_attributes = { // default_attributes can be used to set the 'folder' and 'owner' attributes globally for the template.
+    folder = "DEFAULT"
+    owner  = "100/BOND/AUTOMIC"
+  }
+}
